9a7d9589c3e5e2a3a9d9d29be109249301f80629
darmarzz <123565631+DMarzzucco@users.noreply.github.com>
2024-08-24 17:44:11 -0300

    front base done

--- client/src/page/home.tsx
+++
@@ -1,8 +0,0 @@
-function Home() {
-    return (
-        <section className="flex flex-col justify-center items-center">
-            <div className=" text-2xl font-bold text-blue-500">Home </div>
-        </section>
-    )
-}
-export default Home;
